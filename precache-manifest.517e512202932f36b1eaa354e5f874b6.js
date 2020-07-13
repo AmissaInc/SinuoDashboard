d4c9ef247366dd22f085d203b404079387c5e644
Colby Ford <colbytylerford@gmail.com>
2020-07-12 23:02:36 -0400

    Updates

---
+++ precache-manifest.517e512202932f36b1eaa354e5f874b6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc840f75c80b12b5905b4a08f925f8e",
+    "url": "/SinuoDashboard/index.html"
+  },
+  {
+    "revision": "b7eb3f59aff3fd790850",
+    "url": "/SinuoDashboard/static/css/2.6bbc83f4.chunk.css"
+  },
+  {
+    "revision": "b4fe993cfb89a270f924",
+    "url": "/SinuoDashboard/static/css/main.39de3a1a.chunk.css"
+  },
+  {
+    "revision": "b7eb3f59aff3fd790850",
+    "url": "/SinuoDashboard/static/js/2.b7eb3f59.chunk.js"
+  },
+  {
+    "revision": "b4fe993cfb89a270f924",
+    "url": "/SinuoDashboard/static/js/main.b4fe993c.chunk.js"
+  },
+  {
+    "revision": "1767b82059c9cb622fe5",
+    "url": "/SinuoDashboard/static/js/runtime~main.1767b820.js"
+  },
+  {
+    "revision": "537dd20aca5a2ef744fcca3701994153",
+    "url": "/SinuoDashboard/static/media/404.537dd20a.png"
+  },
+  {
+    "revision": "9908a3be4bd2dc9f1cd407823460cc80",
+    "url": "/SinuoDashboard/static/media/Amissa_Logo_W‪hite.9908a3be.png"
+  },
+  {
+    "revision": "cd3f84e9fabfbe22f8c63edef3e769e7",
+    "url": "/SinuoDashboard/static/media/bg_404.cd3f84e9.png"
+  },
+  {
+    "revision": "fec6134b9773126fdc216a985067485f",
+    "url": "/SinuoDashboard/static/media/circle.fec6134b.svg"
+  },
+  {
+    "revision": "6cd04c4e80726f49e43dd72e2a0ac2c8",
+    "url": "/SinuoDashboard/static/media/logo_dark.6cd04c4e.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/SinuoDashboard/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/SinuoDashboard/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/SinuoDashboard/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/SinuoDashboard/static/media/slick.f97e3bbf.svg"
+  }
+]);
